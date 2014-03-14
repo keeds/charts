6771f60408c4d3c7bee040ea7975d1fc7c1a8227
Andrew Keedle <andrew.keedle@btsgroupuk.com>
2014-03-14 17:02:08 +0000

    chart tweaks

--- src/charts/core.cljs
+++ src/charts/core.cljs
@@ -31,13 +31,11 @@
 ;; ============================================================================
 ;; Utility functions
 
-
 (defn min-value [xs]
   (->>
    xs
    flatten
    (apply min)))
-
 
 (defn max-value [xs]
   (->>
@@ -45,27 +43,25 @@
    flatten
    (apply max)))
 
-
 (defn value-count [xs]
   (->>
    (map count xs)
    (max-value)))
-
 
 ;; (defn xstep [data offset width]
 ;;   (-> width
 ;;       (- (* 2 offset))
 ;;       (/ (value-count data))))
 
+;; (defn index [data]
+;;   (map-indexed #(vec [(+ (* %1 xstep) offset) (+ (* %2 ystep) offset)]) data))
 
-(defn index [data]
-  (map-indexed #(vec [(+ (* %1 xstep) offset) (+ (* %2 ystep) offset)]) data))
-
+(defn index [data xoff yoff cnt]
+  (map-indexed #(vec [(+ (* %1 xoff) offset) (+ (* %2 yoff) offset)]) data))
 
 (defn path [xs]
   (->>
    xs
-   (index)
    (map (fn [[x y]] (str x " " y)))
    (join " L ")
    (str "M ")
@@ -75,25 +71,27 @@
 ;; ============================================================================
 ;; View functions
 
-
 (defn circ
   [c owner]
   (let [[x y] c]
     (om/component
      (dom/circle
       #js {:id "point1"
-            :cx x
-            :cy y
-            :r 4}))))
+           :cx x
+           :cy y
+           :r 4
+           :fill "red"
+           :stroke "black"
+           :strokeWidth 2
+           :strokeOpacity 0.5}))))
 
 (defn points [xs]
   (reify
     om/IRender
     (render
      [state]
-     (let [idx (index xs)]
-       (apply dom/g nil
-              (om/build-all circ idx))))))
+     (apply dom/g nil
+            (om/build-all circ xs)))))
 
 (defn paths
   [app owner]
@@ -105,17 +103,33 @@
          :strokeWidth 2
          :fill "none"})))
 
+(defn translate [xs]
+  (let [min (min-value xs)
+        max (max-value xs)
+        c   (value-count xs)]
+    (map #(index % 50 50 c) xs)))
+
+(defn tx [xs]
+  (let [min (min-value xs)
+        max (max-value xs)
+        c   (value-count xs)]
+    (->> xs
+         (map (fn [x] (map-indexed #(vec [%1 %2]) x)))
+         (map (fx [[x y]] )))))
+
 (defn chart
   [app owner]
   (reify
     om/IRender
     (render
      [this]
-     (apply dom/svg nil
-            (concat
-             (om/build-all paths app)
-             (om/build-all points app)))
+     (let [trans (translate app)]
+       (apply dom/svg nil
+              (concat
+               (om/build-all paths trans)
+               (om/build-all points trans))))
      )))
+
 
 (om/root
  chart
